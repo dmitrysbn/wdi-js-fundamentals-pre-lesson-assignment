c2b0bed781ee4383091dab1c2b5e8b232b528463
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-19 15:57:47 -0400

    Pac-Man program up to and including loops

---
+++ pacman-for-loop.js
@@ -0,0 +1,5 @@
+// pacman-for-loop.js
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde']
+
+for (var index = 0; index < ghosts.length; index++) { console.log('Pac-Man eats ' + ghosts[index]);
+}
---
+++ pacman-objects-2.js
@@ -0,0 +1,11 @@
+// pacman-objects-2.js
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+console.log(ghosts['pinky']);
+console.log(ghosts['clyde']);
+
+var ghostName = 'clyde';
+console.log(ghosts[ghostName]);
